beeba4a4db496af92913b08a3ffd0849a60e45f8
Ana Laura Siczkowski <sicz.ana@hotmail.com>
2023-09-17 19:52:55 -0300

    Agrego borrado logico de turnos

--- internal/turno/repository.go
+++ internal/turno/repository.go
@@ -23,6 +23,7 @@ var (
 	QueryGetById       = `SELECT id, id_odontologo, id_paciente, fecha_hora, descripcion FROM my_db.turnos WHERE id = ?`
 	QueryUpdate        = `UPDATE my_db.turnos SET nombre = ?, apellido = ?, domicilio = ?, dni = ?, alta = ? WHERE id = ?`
 	QueryGetByPaciente = `SELECT id, id_odontologo, id_paciente, fecha_hora, descripcion FROM my_db.turnos WHERE id_paciente = ?`
+	QueryGetByOdontologo = `SELECT id, id_odontologo, id_paciente, fecha_hora, descripcion FROM my_db.turnos WHERE id_odontologo = ?`
 )
 
 // defino la interfaz para que se apliquen siempre todos los métodos
@@ -32,7 +33,8 @@ type Repository interface {
 	CreateTurno(ctx context.Context, p Turno) (Turno, error)
 	UpdateTurno(ctx context.Context, p Turno) (Turno, error)
 	DeleteTurno(ctx context.Context, id int) error
-	GetTurnoByPaciente(ctx context.Context, id int) (Turno, error)
+	GetTurnoByPaciente(ctx context.Context, id int) ([]Turno, error)
+	GetTurnoByOdontologo(ctx context.Context, idOdontolog int) ([]Turno, error)
 }
 
 // estructura repositorio con base de datos mysql
@@ -110,27 +112,81 @@ func (r *repository) GetTurnoByID(ctx context.Context, id int) (Turno, error) {
 }
 
 // obtener turnos por ID del paciente
-func (r *repository) GetTurnoByPaciente(ctx context.Context, id int) (Turno, error) {
+func (r *repository) GetTurnoByPaciente(ctx context.Context, id int) ([]Turno, error) {
 	// ejecuto la query de búsqueda por ID
-	row := r.db.QueryRow(QueryGetByPaciente, id)
+	row,err := r.db.Query(QueryGetByPaciente, id)
+
+	// si hay error de query, lo devuelvo
+	if err != nil {
+		return []Turno{}, ErrEmptyList
+	}
+	defer row.Close()
 
 	// creo la variable que guarde (muestre) el resultado
-	var turno Turno
+	var listadoTurno []Turno
 
 	// verifico si obtengo algún error en los datos
-	err := row.Scan(
-		&turno.ID,
-		&turno.IdOdontologo,
-		&turno.IdPaciente,
-		&turno.FechaHora,
-		&turno.Descripcion,
-	)
-
-	// devuelvo el error o el turno
-	if err != nil {
-		return Turno{}, ErrNotFound
+	for row.Next() {
+		var turno Turno
+		err := row.Scan(
+			&turno.ID,
+			&turno.IdOdontologo,
+			&turno.IdPaciente,
+			&turno.FechaHora,
+			&turno.Descripcion,
+		)
+		if err != nil {
+			return []Turno{}, ErrExec
+		}
+		listadoTurno = append(listadoTurno, turno)
 	}
-	return turno, nil
+	
+	// verifico haber cargado bien todos los registros
+	if err := row.Err(); err != nil {
+		return []Turno{}, ErrExec
+	}
+
+	// devuelvo el resultado
+	return listadoTurno, nil
+}
+
+// obtener turnos por ID del odontolog
+func (r *repository) GetTurnoByOdontologo(ctx context.Context, id int) ([]Turno, error) {
+	// ejecuto la query de búsqueda por ID
+	row,err := r.db.Query(QueryGetByOdontologo, id)
+
+	// si hay error de query, lo devuelvo
+	if err != nil {
+		return []Turno{}, ErrEmptyList
+	}
+	defer row.Close()
+
+	// creo la variable que guarde (muestre) el resultado
+	var listadoTurno []Turno
+
+	// verifico si obtengo algún error en los datos
+	for row.Next() {
+		var turno Turno
+		err := row.Scan(
+			&turno.ID,
+			&turno.IdOdontologo,
+			&turno.IdPaciente,
+			&turno.FechaHora,
+			&turno.Descripcion,
+		)
+		if err != nil {
+			return []Turno{}, ErrExec
+		}
+		listadoTurno = append(listadoTurno, turno)
+	}
+	
+	// verifico haber cargado bien todos los registros
+	if err := row.Err(); err != nil {
+		return []Turno{}, ErrExec
+	}
+
+	// devuelvo el resultado
+	return listadoTurno, nil
 }
 
 // crear turno en BD
